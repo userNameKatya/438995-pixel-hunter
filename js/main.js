228a85de2aa153576dc46c92ca1298c54a08c979
Ekaterina Kondratyeva <ekaterina.kondratyeva@webpp.ru>
2017-05-28 16:33:33 +0300

    Задание #3. Убран Timeout, переименована const

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function () {
   const mainScreen = document.querySelector(`main.central`);
   const arrayTemplates = Array.from(document.querySelectorAll(`template`));
-  const enumeration = {leftArrow: 37, rightArrow: 39};
+  const keyCodes = {leftArrow: 37, rightArrow: 39};
   let activeScreen;
 
   const changeScreen = function (index) {
@@ -9,18 +9,16 @@
     mainScreen.innerHTML = arrayTemplates[index].innerHTML;
   };
 
-  setTimeout(function () {
-    changeScreen(0);
-  }, 1000);
+  changeScreen(0);
 
   document.addEventListener(`keydown`, function (event) {
     if (event.altKey) {
-      if (event.keyCode === enumeration.leftArrow) {
+      if (event.keyCode === keyCodes.leftArrow) {
         if (activeScreen) {
           changeScreen(activeScreen - 1);
         }
         event.preventDefault();
-      } else if (event.keyCode === enumeration.rightArrow) {
+      } else if (event.keyCode === keyCodes.rightArrow) {
         if (activeScreen < arrayTemplates.length - 1) {
           changeScreen(activeScreen + 1);
         }
